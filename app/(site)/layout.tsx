67756675fddcc11a3d0f23678bd0c77e5dd2cc92
Matt Phelps <matt@leandme.com>
2026-05-08 09:23:45 +0200

    Add GA4 gtag script to site layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import AmplitudeInitializer from "../components/AmplitudeInitializer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -11,7 +12,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-base-100 text-base-content">
-      <AmplitudeInitializer />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VQZ4ZPZFJF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VQZ4ZPZFJF');
+          `}
+        </Script>
+        <AmplitudeInitializer />
         <Navbar />
         <main className="container mx-auto px-4 lg:px-8 pb-8 pt-0 min-h-screen">
           {children}
